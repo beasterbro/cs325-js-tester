2fb10e4b223d94e07c5bf9dfe8fb0d23526cdc1d
Adam Stewart <adamjoesph11@gmail.com>
2022-09-23 14:00:25 -0500

    implementing Loops functions using Arrays library

--- solutions/warmup.js
+++ solutions/warmup.js
@@ -1,3 +1,19 @@
 export const sumOfSquares = (x, y) => x*x + y*y;
-export const maxOfThree = (x, y, z) => x > y && x > z ? x : (y > x && y > z ? y : z)//hmm
+export const maxOfThree = (x, y, z) => x > y && x > z ? x : (y > x && y > z ? y : z)
 export const isLeapYear = (x) => (x % 4 == 0 && x % 100 != 0) || x % 400 == 0 ? true : false//modulo funnies
+export const allEvens = (array) =>  array.filter( val => val % 2 == 0)
+export const listSquares = (array) =>  array.flatMap( val => val * val)
+export const listSum = (array) =>  {
+    var sum = 0;
+        array.forEach(val => {
+         sum += val
+    }); 
+    return sum;
+}
+export const listSquaresSum = (array) =>  {
+    var sum = 0;
+        array.forEach(val => {
+         sum += val*val
+    });
+     return sum;
+}
