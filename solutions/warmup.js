1bccc0efc7dfd83542c26b05586bb56e52e94678
Adam Stewart <adamjoesph11@gmail.com>
2022-09-25 16:36:20 -0500

    removed uneccesary comment and ternary, used reduce instead of nested assignment, updated from flatmap to map

--- solutions/warmup.js
+++ solutions/warmup.js
@@ -1,19 +1,7 @@
 export const sumOfSquares = (x, y) => x*x + y*y;
 export const maxOfThree = (x, y, z) => x > y && x > z ? x : (y > x && y > z ? y : z)
-export const isLeapYear = (x) => (x % 4 == 0 && x % 100 != 0) || x % 400 == 0 ? true : false//modulo funnies
+export const isLeapYear = (x) => (x % 4 == 0 && x % 100 != 0) || x % 400 == 0 
 export const allEvens = (array) =>  array.filter( val => val % 2 == 0)
-export const listSquares = (array) =>  array.flatMap( val => val * val)
-export const listSum = (array) =>  {
-    let sum = 0;
-        array.forEach(val => {
-         sum += val
-    }); 
-    return sum;
-}
-export const listSquaresSum = (array) =>  {
-    let sum = 0;
-        array.forEach(val => {
-         sum += val*val
-    });
-     return sum;
-}
+export const listSquares = (array) =>  array.map( val => val * val)
+export const listSum = (array) =>  array.reduce( (prevVal, currVal) => prevVal + currVal, 0);
+export const listSquaresSum = (array) =>  array.reduce( (prevVal, currVal) => prevVal + currVal * currVal, 0);
