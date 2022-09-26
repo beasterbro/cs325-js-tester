f309bfee3a3a6ac85453baf353c99fe16238e3b6
Adam Stewart <adamjoesph11@gmail.com>
2022-09-26 11:06:53 -0500

    commenting mapping classes

--- solutions/mapping.js
+++ solutions/mapping.js
@@ -1,25 +1,24 @@
 export const subArray = (array,indexes) => {
     let result = []
-    indexes.forEach(val => {
-        result.push(array.at(val))
+    indexes.forEach(ind => {
+        result.push(array.at(ind))//find and add values at given indexes
     });
     return result;
 }
 export const getRepeats = (array) => {
-    let tempResult = []
-    let tempArray = []
-    let result = []
-    //tempArray.push(array.at(0))//gives a double virst value
+    let tempResult = []//stores repeat values
+    let tempArray = []//stores values we have seen while iterating
+    let result = []//the tempResult after organizing the data to be in the requested format
     array.forEach(val => {
         
-        if(tempArray.includes(val) && !tempResult.includes(val)){
+        if(tempArray.includes(val) && !tempResult.includes(val)){//if we have seen this value before and have not recorded it as repeat
             tempResult.push(val)
         }
-        else{
+        else{//else record that this value has been seen
             tempArray.push(val)
         }
     });
-    array.forEach(val =>{
+    array.forEach(val =>{//this is a second loop to make sure the data is in the order that it appeared in the original array
         if(tempResult.includes(val) && !result.includes(val)){
             result.push(val)
         }
@@ -29,11 +28,11 @@ export const getRepeats = (array) => {
 //takes in a  array of objects with fields and returns who is over 21
 export const over21 = (array) => array.filter(person => person.age >= 21)
 export const makeHtmlList = (array) => {
-    var resultList = document.createElement("ul");
-    for(let i of array){
+    var resultList = document.createElement("ul");//creating outer unordered list
+    for(let i of array){//loop for creating list items and adding them to the resultList HTML
     let li = document.createElement("li");
     li.innerHTML = i;
     resultList.appendChild(li);
     }
-    return resultList.innerHTML == '' ? resultList.innerHTML : resultList.outerHTML
+    return resultList.innerHTML == '' ? resultList.innerHTML : resultList.outerHTML//ternary for the case where the given list is blank
 }
