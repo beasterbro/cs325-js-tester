42aae36853ca51c9bbe6221f89a2ec3114345111
Adam Stewart <adamjoesph11@gmail.com>
2022-09-26 18:26:44 -0500

    changed makeHtmlList to use strings instead of document objects

--- solutions/mapping.js
+++ solutions/mapping.js
@@ -8,10 +8,7 @@ export const over21 = (array) => array.filter(person => person.age >= 21)
 
 export const makeHtmlList = (array) => {
     var resultList = document.createElement("ul");//creating outer unordered list
-    for(let i of array){//loop for creating list items and adding them to the resultList HTML
-    let li = document.createElement("li");
-    li.innerHTML = i;
-    resultList.appendChild(li);
-    }
+    resultList.innerHTML = array.map( ele => "<li>" + ele + "</li>").join('')
+
     return resultList.innerHTML == '' ? resultList.innerHTML : resultList.outerHTML//ternary for the case where the given list is blank
 }
