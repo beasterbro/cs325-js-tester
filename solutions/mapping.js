ee4754cb8b9ae6ddba32af9a5c3e60fc8291b8ce
Adam Stewart <adamjoesph11@gmail.com>
2022-09-26 18:20:38 -0500

    made requested changes to functions

--- solutions/mapping.js
+++ solutions/mapping.js
@@ -1,32 +1,11 @@
-export const subArray = (array,indexes) => {
-    let result = []
-    indexes.forEach(ind => {
-        result.push(array.at(ind))//find and add values at given indexes
-    });
-    return result;
-}
-export const getRepeats = (array) => {
-    let tempResult = []//stores repeat values
-    let tempArray = []//stores values we have seen while iterating
-    let result = []//the tempResult after organizing the data to be in the requested format
-    array.forEach(val => {
-        
-        if(tempArray.includes(val) && !tempResult.includes(val)){//if we have seen this value before and have not recorded it as repeat
-            tempResult.push(val)
-        }
-        else{//else record that this value has been seen
-            tempArray.push(val)
-        }
-    });
-    array.forEach(val =>{//this is a second loop to make sure the data is in the order that it appeared in the original array
-        if(tempResult.includes(val) && !result.includes(val)){
-            result.push(val)
-        }
-    });
-    return result;
-}
+export const subArray = (array,indexes) => indexes.map( ind => array.at(ind))//array.filter(val => indexes.includes(array.indexOf(val)))
+
+//returns true if it is not the last(or the  only) index in the array storing its value 
+export const getRepeats = (array) => array.filter( (val, ind, array) => (ind < array.findLastIndex( (ele) => ele == val ) && ind == array.indexOf(val)) )
+
 //takes in a  array of objects with fields and returns who is over 21
 export const over21 = (array) => array.filter(person => person.age >= 21)
+
 export const makeHtmlList = (array) => {
     var resultList = document.createElement("ul");//creating outer unordered list
     for(let i of array){//loop for creating list items and adding them to the resultList HTML
