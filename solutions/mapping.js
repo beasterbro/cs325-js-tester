5adfe281504b820c9b9a1dbb9e6d3a41c26fb177
Adam Stewart <adamjoesph11@gmail.com>
2022-09-26 20:48:48 -0500

    adjusting formatting for readability

--- solutions/mapping.js
+++ solutions/mapping.js
@@ -1,4 +1,5 @@
 export const subArray = (array,indexes) => indexes.map( ind => array.at(ind))
+
 //returns true if it is not the last(or the  only) index in the array storing its value 
 export const getRepeats = (array) => array.filter( (val, ind) =>
      (ind < array.lastIndexOf( val ) && ind == array.indexOf(val)) )
@@ -7,8 +8,8 @@ export const getRepeats = (array) => array.filter( (val, ind) =>
 export const over21 = (array) => array.filter(person => person.age >= 21)
 
 export const makeHtmlList = (array) => {
-    var resultList = ""
-    resultList = "<ul><li>" + array.join('</li><li>') + "</li></ul>" //using strings instead of create document
+    var resultList = ""//resultList now a string instead of using createElement
+    resultList = "<ul><li>" + array.join('</li><li>') + "</li></ul>" //using strings instead of createElement
 
     return resultList == "<ul><li></li></ul>" ? '' : resultList//ternary for the case where the given list is blank
 }
