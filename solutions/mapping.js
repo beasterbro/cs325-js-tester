bf34ac0b9e9e7f10f06e2e4dd0d282cafe05ed9a
Adam Stewart <adamjoesph11@gmail.com>
2022-09-26 20:47:09 -0500

    makeHTML now uses ONLY strings

--- solutions/mapping.js
+++ solutions/mapping.js
@@ -1,14 +1,14 @@
-export const subArray = (array,indexes) => indexes.map( ind => array.at(ind))//array.filter(val => indexes.includes(array.indexOf(val)))
-
+export const subArray = (array,indexes) => indexes.map( ind => array.at(ind))
 //returns true if it is not the last(or the  only) index in the array storing its value 
-export const getRepeats = (array) => array.filter( (val, ind, array) => (ind < array.findLastIndex( (ele) => ele == val ) && ind == array.indexOf(val)) )
+export const getRepeats = (array) => array.filter( (val, ind) =>
+     (ind < array.lastIndexOf( val ) && ind == array.indexOf(val)) )
 
 //takes in a  array of objects with fields and returns who is over 21
 export const over21 = (array) => array.filter(person => person.age >= 21)
 
 export const makeHtmlList = (array) => {
-    var resultList = document.createElement("ul");//creating outer unordered list
-    resultList.innerHTML = array.map( ele => "<li>" + ele + "</li>").join('')//using strings instead of create document
+    var resultList = ""
+    resultList = "<ul><li>" + array.join('</li><li>') + "</li></ul>" //using strings instead of create document
 
-    return resultList.innerHTML == '' ? resultList.innerHTML : resultList.outerHTML//ternary for the case where the given list is blank
+    return resultList == "<ul><li></li></ul>" ? '' : resultList//ternary for the case where the given list is blank
 }
