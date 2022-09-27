ac7d8030e9438f5c012c3af115fb46db878359d6
Adam Stewart <adamjoesph11@gmail.com>
2022-09-26 20:54:37 -0500

    it is now it its final form:tm:

--- solutions/mapping.js
+++ solutions/mapping.js
@@ -2,7 +2,7 @@ export const subArray = (array,indexes) => indexes.map( ind => array.at(ind))
 
 //returns true if it is not the last(or the  only) index in the array storing its value 
 export const getRepeats = (array) => array.filter( (val, ind) =>
-     (ind < array.lastIndexOf( val ) && ind == array.indexOf(val)) )
+     (ind < array.lastIndexOf(val) && ind == array.indexOf(val)) )
 
 //takes in a  array of objects with fields and returns who is over 21
 export const over21 = (array) => array.filter(person => person.age >= 21)
